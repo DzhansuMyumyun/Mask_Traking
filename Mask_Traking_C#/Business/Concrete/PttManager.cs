7fb6da8ac166987f87b0e1840b364b997d4a69fc
Dzhansu <167971328+DzhansuMyumyun@users.noreply.github.com>
2024-04-26 12:47:43 +0200

    The project is added with updates.

---
+++ Mask_Traking_C#/Business/Concrete/PttManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        //dependecy injection
+        private IApplicantService _applicanService; //reference holder
+
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicanService = applicantService;
+        }
+
+        public void GiveMask(Person person)
+        {
+
+            if(_applicanService.CheckPerson(person)) 
+            {
+                Console.WriteLine( " The mask is given to " + person.Firstname );
+            }
+            else
+            {
+                Console.WriteLine("The mask is not given");
+            }
+            
+        }
+
+        public void GiveMask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mask_Traking_C#/Workaround/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+            person.Nationality = 123;
+            person.Firstname = "Zehra";
+            person.Lastname = "Myumyun";
+            person.DateOfBirthYear = 098789;
+
+            Console.WriteLine(person.Firstname);
+
+            PttManager pttManager = new PttManager(new PersonManager ());
+            pttManager.GiveMask(person);
+
+        }
+        
+    }
+
+}
